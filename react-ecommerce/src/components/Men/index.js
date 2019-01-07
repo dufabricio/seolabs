794a7442b9e7f03eb669c8b48b23d2f1edcd80ca
eduardo <eduardo.fabricio@superbid.net>
2019-01-07 15:52:59 -0200

    including datalayer values

--- react-ecommerce/src/components/Men/index.js
+++ react-ecommerce/src/components/Men/index.js
@@ -4,14 +4,20 @@ import React from 'react';
 import MenItems from './MenItems';
 import './index.css';
 
-const MensProducts = () => (
-  <div className="mens-products">
-    <div className="mens-title">
-      <h4>Men's Items</h4>
-    </div>
-    <MenItems />
-  </div>
-);
+
+class MensProducts extends Component {
+
+  render() {
+    return(
+      <div className="mens-products">
+        <div className="mens-title">
+          <h4>Men's Items</h4>
+        </div>
+        <MenItems />
+      </div>
+    );
+  }
+}
 
 
 export default MensProducts;
